db4af0fabd14325462eed305474c50abd0d24106
rasheed197 <94674581+rasheed197@users.noreply.github.com>
2024-07-08 23:17:31 +0100

    Delete userauth directory

--- userauth/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import UserRegistrationView, UserLoginView, OrganisationListView, CreatedOrganisationsView
-
-urlpatterns = [
-    path('auth/register/', UserRegistrationView.as_view(), name='register'),
-    path('auth/login/', UserLoginView.as_view(), name='login'),
-    path('api/created-organisations/', CreatedOrganisationsView.as_view(), name='created-organisations'),
-]
